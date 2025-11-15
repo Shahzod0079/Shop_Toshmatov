1d48dc41360b0f72ae617827725d16fbf1865514
student-a502 <student-a502@STUD-RM11-A502>
2025-11-15 11:56:50 +0500

    Разработанный класс Children. Тошматов

---
+++ Classes/Children.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Toshmatov.Classes
+{
+    public class Children : Shop
+    {
+
+        public int Age { get; set; }
+
+        public Children(string name, int price, int age) : base(name, price)
+        {
+            Age = age;
+        }
+    }
+}
