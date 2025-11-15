9b1726a10f9f4ec041d239097a6d38ea5dfe2540
student-a502 <student-a502@STUD-RM11-A502>
2025-11-15 11:54:29 +0500

    Разработанный класс Sport. Тошматов

---
+++ Classes/Sport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Toshmatov.Classes
+{
+    public class Sport : Shop
+    {
+        public string Size { get; set; }
+
+        public Sport(string name, int price, string size) : base(name, price)
+        {
+            Size = size;
+        }
+    }
+}
